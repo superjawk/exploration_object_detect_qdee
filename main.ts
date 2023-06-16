b18a0b8e0c4a24e195292170bd903f7a98d1c27f
superjawk <103614651+superjawk@users.noreply.github.com>
2023-06-16 11:58:36 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -46,17 +46,23 @@ function colortofacial () {
     }
 }
 function servo (servo1: number, servo2: number) {
-    // B1,B2:  use for the servo motor to grab the object
-    if ((0 as any) == ("B1" as any)) {
-        qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, servo1, 1000)
-        qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, servo2, 1000)
-        basic.showString("B1")
-        basic.pause(1000)
-    }
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, servo1, 1000)
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, servo2, 1000)
+    basic.showString("B1")
+    basic.pause(1000)
 }
-function lineFollow () {
+function servoInit () {
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 30, 1000)
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -30, 1000)
+    basic.showString("B1")
+    basic.pause(1000)
+}
+function lineFollow (mode: string) {
     // B1,B2:  use for the servo motor to grab the object
-    if ((0 as any) == ("B2" as any)) {
+    if (mode == "B2") {
+        lineFollowMode += 1
+    }
+    if (lineFollowMode % 2 == 1) {
         if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_OUT_S2_OUT)) {
             qdee.qdee_setMotorSpeed(0, 0)
         } else if (qdee.qdee_readLineFollowerStatus(qdee.lineFollowPort.port1, qdee.qdee_lineFollower.S1_IN_S2_IN)) {
@@ -70,17 +76,21 @@ function lineFollow () {
 }
 radio.onReceivedString(function (receivedString) {
     move(receivedString)
-    servoSwitch()
-    lineFollow()
+    servoSwitch(receivedString)
+    lineFollow(receivedString)
     colortofacial()
+    lineFollow(receivedString)
 })
-function servoSwitch () {
-    if (servoSwitchFlag % 2 == 0) {
-        servo(-30, 30)
-    } else {
-        servo(30, -30)
+function servoSwitch (command: string) {
+    // B1,B2:  use for the servo motor to grab the object
+    if (command == "B1") {
+        if (servoSwitchFlag % 2 == 0) {
+            servo(-30, 30)
+        } else {
+            servo(30, -30)
+        }
+        servoSwitchFlag += 1
     }
-    servoSwitchFlag += 1
 }
 function move (command: string) {
     if (command == "left") {
@@ -101,6 +111,7 @@ function move (command: string) {
             . . . # .
             . . . . .
             `)
+        qdee.qdee_setMotorSpeed(left_speed, 0 - right_speed)
     }
     if (command == "forward") {
         basic.showLeds(`
@@ -110,6 +121,7 @@ function move (command: string) {
             . . . . .
             . . . . .
             `)
+        qdee.qdee_setMotorSpeed(left_speed, right_speed)
     }
     if (command == "backward") {
         basic.showLeds(`
@@ -119,8 +131,57 @@ function move (command: string) {
             . # . # .
             . . # . .
             `)
+        qdee.qdee_setMotorSpeed(0 - left_speed, 0 - right_speed)
     }
 }
+function colorToAprilTag () {
+    wondercam.UpdateResult()
+    if (wondercam.IsDetectedColorblobs()) {
+        if (wondercam.isDetectedColorId(1)) {
+            if (wondercam.XOfColorId(wondercam.Options.Width, 1) * wondercam.XOfColorId(wondercam.Options.Height, 1) > 4000) {
+                qdee.qdee_setPixelRGB(QdeeLights.All, QdeeRGBColors.Red)
+                qdee.qdee_showLight()
+                basic.showString("R")
+                wondercam.ChangeFunc(wondercam.Functions.AprilTag)
+            } else {
+                qdee.qdee_clearLight()
+                basic.clearScreen()
+            }
+        } else if (wondercam.isDetectedColorId(2)) {
+            if (wondercam.XOfColorId(wondercam.Options.Width, 2) * wondercam.XOfColorId(wondercam.Options.Height, 2) > 4000) {
+                qdee.qdee_setPixelRGB(QdeeLights.All, QdeeRGBColors.Yellow)
+                qdee.qdee_showLight()
+                basic.showString("Y")
+                wondercam.ChangeFunc(wondercam.Functions.AprilTag)
+            } else {
+                qdee.qdee_clearLight()
+                basic.clearScreen()
+            }
+        } else {
+            if (wondercam.isDetectedColorId(3)) {
+                if (wondercam.XOfColorId(wondercam.Options.Width, 3) * wondercam.XOfColorId(wondercam.Options.Height, 3) > 4000) {
+                    qdee.qdee_setPixelRGB(QdeeLights.All, QdeeRGBColors.Green)
+                    qdee.qdee_showLight()
+                    basic.showString("G")
+                    wondercam.ChangeFunc(wondercam.Functions.AprilTag)
+                } else {
+                    qdee.qdee_clearLight()
+                    basic.clearScreen()
+                }
+            }
+        }
+    } else if (wondercam.isDetectedAprilTag()) {
+        if (wondercam.isDetecteAprilTagId(1)) {
+            basic.showString("tag1")
+        } else {
+            basic.clearScreen()
+        }
+    } else {
+        qdee.qdee_clearLight()
+        basic.clearScreen()
+    }
+}
+let lineFollowMode = 0
 let servoSwitchFlag = 0
 let right_speed = 0
 let left_speed = 0
@@ -128,5 +189,7 @@ qdee.qdee_Init()
 left_speed = 100
 right_speed = 100
 servoSwitchFlag = 1
+lineFollowMode = 0
 wondercam.wondercam_init(wondercam.DEV_ADDR.x32)
 wondercam.ChangeFunc(wondercam.Functions.ColorDetect)
+servoInit()
